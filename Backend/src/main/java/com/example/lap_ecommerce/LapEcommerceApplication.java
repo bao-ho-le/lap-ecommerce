75a6ed19160a89434506a8f5702be6159c97d6c3
Itsmennguyen <tranduy2615@gmail.com>
2026-05-13 11:25:58 +0700

    .

---
+++ Backend/src/main/java/com/example/lap_ecommerce/LapEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.lap_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LapEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LapEcommerceApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/example/lap_ecommerce/LapEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lap_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LapEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
