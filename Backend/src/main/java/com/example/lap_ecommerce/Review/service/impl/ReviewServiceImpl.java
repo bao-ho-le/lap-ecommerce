5c7d32624d6b9fb146264fac7b21a1c4a8f1645f
Itsmennguyen <tranduy2615@gmail.com>
2026-06-23 08:51:25 +0700

    .

--- Backend/src/main/java/com/example/lap_ecommerce/Review/service/impl/ReviewServiceImpl.java
+++ Backend/src/main/java/com/example/lap_ecommerce/Review/service/impl/ReviewServiceImpl.java
@@ -1,8 +1,8 @@
 package com.example.lap_ecommerce.Review.service.impl;
 
-import com.example.lap_ecommerce.Order.service.OrderService;
-import com.example.lap_ecommerce.Product.entity.Product;
-import com.example.lap_ecommerce.Product.repository.ProductRepository;
+import com.example.lap_ecommerce.order.service.OrderService;
+import com.example.lap_ecommerce.product.entity.Product;
+import com.example.lap_ecommerce.product.repository.ProductRepository;
 import com.example.lap_ecommerce.Review.dto.request.CreateReviewRequest;
 import com.example.lap_ecommerce.Review.dto.response.CreateReviewResponse;
 import com.example.lap_ecommerce.Review.dto.response.ProductReviewResponse;
